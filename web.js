558894907e82ca61ad0cf39d704f327417538ebb
Daniel Boiarsky <boiarsky18@gmail.com>
2013-07-14 21:39:06 +0000

    index.html addition 5

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
 var fs = require('fs'); 
-var buf = fs.readFileSync('/index.html');
+
 
 response.send('Hello');
 });
