761f4f1ef0514e3fd460652f0a9944e46505bc2a
Daniel Boiarsky <boiarsky18@gmail.com>
2013-07-14 20:45:06 +0000

    index.html addition

--- web.js
+++ web.js
@@ -1,10 +1,11 @@
 var express = require('express');
+var fs = require('index.html');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
-});
+  response.send(bu.tosString(fs.readFile('index.html')));
+
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
